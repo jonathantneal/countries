0ea022b1fe0dcc2570e94f78555c37fb49591f5a
Jonathan Neal <jonathantneal@hotmail.com>
2013-09-23 18:53:12 -0700

    countries
    countries.php service
    index.php implementation
    .htaccess minimal modification

--- countries.php
+++ countries.php
@@ -1,53 +1,57 @@
 <?php
 
-function array_slice_assoc(&$array, $key) {
-	$return = @$array[$key];
+namespace Countries;
 
-	unset($array[$key]);
+const JSON_PATH = 'countries.json';
 
-	return $return;
-}
+function GetData($SETTING = array()) {
+	$INPUT_LIST = json_decode(file_get_contents(JSON_PATH));
+	$INPUT_KEYS = array_keys((array) $INPUT_LIST[0]);
 
-$COUNTRY_LIST = json_decode(file_get_contents('countries.json'));
-$RETURN_LIST  = isset($_GET['return']) && is_array($_GET['return']) ? $_GET['return'] : array_keys((array) $COUNTRY_LIST[0]); unset($_GET['return']);
-$RETURN_DATA  = array();
+	$KEY_LIST = array_intersect_key($SETTING, array_flip($INPUT_KEYS));
+	$PROPERTY_LIST = is_array(@$SETTING['return']) ? array_flip(array_intersect_key(array_flip($SETTING['return']), array_flip($INPUT_KEYS))) : $INPUT_KEYS;
 
-$IS_ARRAY    = false;
-$IS_KEYLESS  = isset($_GET['keyless']); unset($_GET['keyless']);
-$IS_READABLE = isset($_GET['readable']); unset($_GET['readable']);
+	$IS_ARRAY = empty($KEY_LIST);
+	$IS_KEYLESS  = isset($SETTING['keyless']);
 
-$CALLBACK = isset($_GET['callback']) ? $_GET['callback'] : false; unset($_GET['callback']);
+	$RETURN_LIST = array();
 
-if (count($_GET) < 1) $_GET['return'] = array();
+	foreach ($INPUT_LIST as $INPUT) {
+		foreach ($KEY_LIST as $KEY => $VALUE) {
+			if (is_array($VALUE)) $IS_ARRAY = true;
+			elseif (!empty($VALUE)) $VALUE = array($VALUE);
 
-foreach ($_GET as $KEY => $value) {
-	if (is_array($value)) $IS_ARRAY = true;
-	else $value = array($value);
-
-	foreach ($COUNTRY_LIST as $index => $country) {
-		if ($KEY === 'return' || (isset($country->{$KEY}) && in_array($country->{$KEY}, $value))) {
-			$RETURN_EACH = array();
-
-			foreach ($RETURN_LIST as $include) {
-				if ($IS_KEYLESS) {
-				 	$RETURN_EACH[] = $country->$include;
-				} else {
-				 	$RETURN_EACH[$include] = $country->$include;
-				}
+			if (!empty($VALUE) && isset($INPUT->{$KEY}) && !in_array($INPUT->{$KEY}, $VALUE)) {
+				continue 2;
 			}
-
-			$RETURN_DATA[] = $RETURN_EACH;
 		}
+
+		$RETURN_EACH = array();
+
+		foreach ($PROPERTY_LIST as $PROPERTY) {
+			if ($IS_KEYLESS) $RETURN_EACH[] = $INPUT->$PROPERTY;
+			else $RETURN_EACH[$PROPERTY] = $INPUT->$PROPERTY;
+		}
+
+		$RETURN_LIST[] = $RETURN_EACH;
 	}
+
+	return $IS_ARRAY ? $RETURN_LIST : @$RETURN_LIST[0];
 }
 
-header('Content-Type: '.($CALLBACK ? 'application/json' : 'application/javascript'));
+function GetJSON($SETTING = array()) {
+	$IS_READABLE = isset($SETTING['readable']);
 
-print(
-	($CALLBACK ? $CALLBACK.'(' : '').
-	json_encode(
-		$IS_ARRAY ? $RETURN_DATA : @$RETURN_DATA[0],
-		$IS_READABLE ? JSON_PRETTY_PRINT : null
-	).
-	($CALLBACK ? $CALLBACK.');' : '')
-);
+	$CALLBACK = isset($SETTING['callback']) ? $SETTING['callback'] : false;
+	$MIMETYPE = $CALLBACK ? 'application/json' : 'application/javascript';
+
+	$JSON = GetData($SETTING);
+
+	header('Content-Type: '.$MIMETYPE);
+
+	if ($CALLBACK) print($CALLBACK.'(');
+
+	print(json_encode($JSON, $IS_READABLE ?  JSON_PRETTY_PRINT : null));
+
+	if ($CALLBACK) print(');');
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once('countries.php');
+
+Countries\GetJSON($_GET);
